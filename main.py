3318f6e80daed87ab3288c767c58201251b81905
CameronTrejo2000 <camerontrejo2000@gmail.com>
2021-04-14 08:59:46 -0600

    Added main program

---
+++ main.py
@@ -0,0 +1,13 @@
+
+from Card import Card
+
+def main():
+    acornBearer = Card('Acorn Bearer', 1)
+    deathwingTheDestroyer = Card('Deathwing The Destroyer', 10)
+
+    acornBearer.printCard()
+    deathwingTheDestroyer.printCard()
+
+
+if __name__ == '__main__':
+    main()
